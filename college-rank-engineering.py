75f04a045166f0b4b81b7f4e67bb180091fc585c
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-09-03 01:06:38 +0530

    Create college-rank-engineering.py

---
+++ college-rank-engineering.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import html5lib
+s=requests.get('https://aglasem.com/top-engineering-colleges-in-india/')
+soup=BeautifulSoup(s.content,'html5lib')
+a=soup.find('table')
+y=[]
+h={}
+for i in range(1,200):
+  b=a.find_all('tr')[i]
+  rank=b.find_all('td')[0].getText()
+  college=b.find_all('td')[1].getText()
+  state=b.find_all('td')[3].getText()
+  da={"rank":rank,"college":college,"state":state}
+  y=y+[da]
+print(y)
